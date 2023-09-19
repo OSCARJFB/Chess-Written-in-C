d4b6ab1cde0938533b18da5127b534331b772522
OSCARJFB <mrsnask@hotmail.com>
2023-09-19 20:56:09 +0200

    Refactor chess_graphics.c

--- src/chess_graphics.c
+++ src/chess_graphics.c
@@ -1,9 +1,9 @@
 /*
-   	Writen by: Oscar Bergström
-	https://github.com/OSCARJFB
+   Writen by: Oscar Bergström
+https://github.com/OSCARJFB
 
-	MIT License
-	Copyright (c) 2023 Oscar Bergström
+MIT License
+Copyright (c) 2023 Oscar Bergström
 */
 
 #include "chess_graphics.h"
@@ -16,111 +16,166 @@ void initGraphics(void)
 	SetTargetFPS(30); 
 }
 
-static inline Vector2 selectPiece(bool isPressed, Vector2 coordinates)
-{
-	if(!isPressed)
-	{
-		Vector2 outOfBoard = {-1, -1};
-		return outOfBoard; 
-	}
-	return coordinates;
-}
-
-/**
- * Here we print a piece at a location, 
- * This is dependant on the char (piece type) int (coordinates) paramters. 
- * TODO Add sprites / models for each piece type instead using primitive symbols. 
- */
-static inline void drawPiece(int x, int y, char piece)
-{
-	int size = 25; ; 
-	Color p1 = {150, 55, 0, 255}, p2 = {255, 200, 150, 255}; 
-	switch(piece)
-	{
-		case 'P':
-			DrawText("P", x, y, size, p1); 
-			break; 
-		case 'p':
-			DrawText("P", x, y, size, p2); 
-			break; 
-		case 'R':
-			DrawText("R", x, y, size, p1); 
-			break; 
-		case 'r':
-			DrawText("r", x, y, size, p2); 
-			break; 
-		case 'K':
-			DrawText("K", x, y, size, p1); 
-			break;
-		case 'k':
-			DrawText("k", x, y, size, p2); 
-			break;
-		case 'B':
-			DrawText("B", x, y, size, p1); 
-			break; 
-		case 'b':
-			DrawText("b", x, y, size, p2); 
-			break; 
-		case 'Q':
-			DrawText("Q", x, y, size, p1); 
-			break; 
-		case 'q':
-			DrawText("q", x, y, size, p2); 
-			break;
-		case 'W':
-			DrawText("W", x, y, size, p1); 
-			break; 
-		case 'w':
-			DrawText("w", x, y, size, p2); 
-			break;
-	}
-}
-
 /**
  * This function will draw the board including the pieces at their location.
  * At the moment it will also handle movement and game logic. This should be split later on.
  */
-void drawBoard(char chessBoard[8][8], move m_data)
+void drawBoard(void)
 {
 	int x = 0, y = 0; 
 	int size = 40; 
-	bool squareColor = true, is_Selected = false;
-	Vector2 s_coordinates = {-1, -1}, m_coordinates = {-1, -1};
-	Color black = {0, 0, 0, 255}, white = {255, 255, 255, 255}, green = {0, 255, 0, 255};
+	bool squareColor = true; 
 
-	while(!WindowShouldClose())
+	for(int i = 0; i < 8; ++i)
 	{
-		BeginDrawing();	
-
-		if(IsMouseButtonPressed(0) && !is_Selected)
+		for(int j = 0; j < 8; ++j)
 		{
-			s_coordinates = GetMousePosition(); 
-		}	
+			squareColor == true ? DrawRectangle(x, y, size, size, CHESS_WHITE) : DrawRectangle(x, y, size, size, CHESS_BLACK);
+			x += size; 
+			squareColor = squareColor == true ? false : true;
 
-		if(IsMouseButtonPressed(0) && is_Selected)
-		{
-			m_coordinates = GetMousePosition();	
 		}
+		squareColor = squareColor == true ? false : true;
+		y += size; 
+		x = 0; 
+	}
+}
 
-		for(int i = 0; i < 8; ++i)
+/**
+ * This function will print all the pieces.
+ */
+static void drawPieces(char chessBoard[8][8])
+{
+	int x = 0, y = 0;
+	int size = 40; 
+
+	for(int i = 0; i < 8; ++i)
+	{
+		for(int j = 0; j < 8; ++j)
 		{
-			for(int j = 0; j < 8; ++j)
+			switch(chessBoard[i][j])
 			{
-				squareColor == true ? DrawRectangle(x, y, size, size, white) : DrawRectangle(x, y, size, size, black);
 
-				if (s_coordinates.x > x && s_coordinates.x < x + size &&
-						s_coordinates.y > y && s_coordinates.y < y + size)
-				{	
+				case 'P':
+					DrawText("P", x, y, size, CHESS_P1); 
+					break; 
+				case 'p':
+					DrawText("P", x, y, size, CHESS_P2); 
+					break; 
+				case 'R':
+					DrawText("R", x, y, size, CHESS_P1); 
+					break; 
+				case 'r':
+					DrawText("r", x, y, size, CHESS_P2); 
+					break; 
+				case 'K':
+					DrawText("K", x, y, size, CHESS_P1); 
+					break;
+				case 'k':
+					DrawText("k", x, y, size, CHESS_P2); 
+					break;
+				case 'B':
+					DrawText("B", x, y, size, CHESS_P1); 
+					break; 
+				case 'b':
+					DrawText("b", x, y, size, CHESS_P2); 
+					break; 
+				case 'Q':
+					DrawText("Q", x, y, size, CHESS_P1); 
+					break; 
+				case 'q':
+					DrawText("q", x, y, size, CHESS_P2); 
+					break;
+				case 'W':
+					DrawText("W", x, y, size, CHESS_P1); 
+					break; 
+				case 'w':
+					DrawText("w", x, y, size, CHESS_P2); 
+					break;
+			}
+			x += size; 
+		}
+		y += size; 
+		x = 0;
+	}
+}
+
+static inline Vector2 readMouseClick(void)
+{
+	if(IsMouseButtonPressed(0))
+	{
+		return GetMousePosition(); 
+	}
+
+	return (Vector2){-1, -1}; // No valid selection. 
+}
+
+void getMove(Vector2 **source, Vector2 **destination, Vector2 mouseClick)
+{
+	if(mouseClick.x == -1 && mouseClick.y == -1)
+	{
+		return; 
+	}
+
+	if(*source == NULL)
+	{
+		*source = malloc(sizeof(Vector2)); 
+		if(*source == NULL)
+		{
+			return; 
+		}
+		(*source)->x = mouseClick.x; 
+		(*source)->y = mouseClick.y;
+		return; 
+	}
+
+	if(*destination == NULL)
+	{
+		*destination = malloc(sizeof(Vector2)); 
+		if(*destination == NULL)
+		{
+			return; 
+		}
+		(*destination)->x = mouseClick.x; 
+		(*destination)->y = mouseClick.y;
+	}
+}
+
+static inline void deleteChessMove(Vector2 **source, Vector2 **destination)
+{
+	if(*source != NULL && *destination != NULL)
+	{
+		free(*source);
+		free(*destination); 
+		*source = *destination = NULL; 
+	}
+}
+
+static move drawAndExecuteMove(Vector2 *source, Vector2 *destination, move m_data, char chessBoard[8][8])
+{
+	int size = 40; 
+	int x = 0, y = 0; 
+
+	for(int i = 0; i < 8; ++i)
+	{
+		for(int j = 0; j < 8; ++j)
+		{
+
+			if(source != NULL)
+			{
+				if (source->x > x && source->x < x + size && source->y > y && source->y < y + size)
+				{
 					m_data.y_sel = i; 
 					m_data.x_sel = j; 
-					DrawRectangle(x, y, size, size, green);
-					is_Selected = true; 
+					DrawRectangle(x, y, size, size, CHESS_GREEN);
 				}
+			}
 
-				drawPiece(x + 5, y + 5, chessBoard[i][j]); 
+			// here we want to add a functionality to when hoovering to check if move is available. 
 
-				if (is_Selected && m_coordinates.x > x && m_coordinates.x < x + size &&
-						m_coordinates.y > y && m_coordinates.y < y + size)
+			if(destination != NULL)
+			{
+				if (destination->x > x && destination->x < x + size && destination->y > y && destination->y < y + size)
 				{
 					m_data.y_mov = i;
 					m_data.x_mov = j; 
@@ -128,30 +183,31 @@ void drawBoard(char chessBoard[8][8], move m_data)
 					m_data = castlingController(chessBoard, m_data);
 					m_data = executeMove(chessBoard, m_data);
 					m_data = enPassant(chessBoard, m_data);
-					s_coordinates.x = s_coordinates.y = m_coordinates.x = m_coordinates.y = -1; 
-					is_Selected = false; 
 				}
-
-				x += size; 
-				squareColor = squareColor == true ? false : true;
-
 			}
-			squareColor = squareColor == true ? false : true;
-			y += size; 
-			x = 0; 
+			x += size; 
 		}
+		y += size; 
+		x = 0;
+	}
+	return m_data; 
+}
 
-		if (checkmate(chessBoard, m_data))
-		{
-
-			DrawText("Checkmate", x, (y + 1) * size, size, white);
-			DrawText("Would you like to play again? (y/n)", x, (y + 2) * size, size, white);
-			// do something here
-		}
-
-		x = y = 0;
+void gameLoop(char chessBoard[8][8], move m_data)
+{
+	Vector2 *source = NULL, *destination = NULL;
+	initGraphics(); 
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();	
+		drawBoard();	
+		Vector2 mouseClick = readMouseClick(); 
+		getMove(&source, &destination, mouseClick);
+		m_data = drawAndExecuteMove(source, destination, m_data, chessBoard);
+		deleteChessMove(&source, &destination);
+		drawPieces(chessBoard);
 		EndDrawing(); 
 	}
 
-	CloseWindow(); 
 }
+
--- src/chess_graphics.h
+++ src/chess_graphics.h
@@ -2,11 +2,17 @@
 #define CHESS_GRAPHICS_H
 
 #include <raylib.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "chess_mvData.h"
 #include "chess_engine.h"
 
-void initGraphics(void); 
-void drawBoard(char chessBoard[8][8], move m_data); 
+#define CHESS_BLACK (Color){0, 0, 0, 255} 
+#define CHESS_WHITE (Color){255, 255, 255, 255} 
+#define CHESS_GREEN (Color){0, 255, 0, 255}
+#define CHESS_P1 (Color){150, 55, 0, 255}
+#define CHESS_P2 (Color){255, 200, 150, 255}
+
+void gameLoop(char chessBoard[8][8], move m_data);
 
 #endif  // CHESS_GRAPHICS_H
